96c950542cc4a21d2c281b79ab0d90e1fac25603
Klaster_1 <Klaster1@gmail.com>
2017-10-09 21:20:21 +0700

    #2 Restore removeNewLink option and feature.

--- content.js
+++ content.js
@@ -1,16 +1,43 @@
-const seriesParser = new MUSeriesParser(window.DOMParser)
-const listObserver = new MUListObserver(document, window.MutationObserver)
+const main = async () => {
+	const seriesParser = new MUSeriesParser(window.DOMParser)
+	const listObserver = new MUListObserver(document, window.MutationObserver)
+	const options = await browser.storage.local.get(defaults)
 
-const checkSeries = () => {
-	try {
-		const updatedSeries = seriesParser.parseUpdatedSeries(document)
-		browser.runtime.sendMessage({type: 'series', payload: updatedSeries})
-		console.log(updatedSeries)
-	} catch (e) {
-		browser.runtime.sendMessage({type: 'error', payload: e.message})
-		console.error(e)
+	browser.storage.onChanged.addListener(changes => {
+		if (
+			'removeNewLink' in changes &&
+			changes.removeNewLink.oldValue !== changes.removeNewLink.newValue
+		) {
+			options.removeNewLink = changes.removeNewLink.newValue
+			if (changes.removeNewLink.newValue) syncChapterStatus()
+		}
+	})
+
+	const checkSeries = () => {
+		try {
+			const updatedSeries = seriesParser.parseUpdatedSeries(document)
+			browser.runtime.sendMessage({type: 'series', payload: updatedSeries})
+		} catch (e) {
+			browser.runtime.sendMessage({type: 'error', payload: e.message})
+			console.error(e)
+		}
 	}
-}
 
-checkSeries()
-listObserver.observe(MUSeriesParser.LIST_SELECTOR, checkSeries)
+	const chnum = el => +el.textContent.match(/\d+/)[0]
+	const syncChapterStatus = () => document.querySelectorAll(`${MUSeriesParser.LIST_SELECTOR} tr[id]`)
+	.forEach(row => {
+		const updateLink = row.querySelector('.newlist')
+		if (!updateLink) return
+		const chapter = chnum(row.querySelector('a[title="Increment Chapter"]'))
+		const update = chnum(updateLink)
+		if (chapter >= update) updateLink.remove()
+	})
+
+	checkSeries()
+
+	listObserver.observe(MUSeriesParser.LIST_SELECTOR, () => {
+		if (options.removeNewLink) syncChapterStatus()
+		checkSeries()
+	})
+}
+main()
--- options.js
+++ options.js
@@ -1,4 +1,5 @@
 const updateIntervalElement = document.querySelector('#updateInterval')
+const removeNewLink = document.querySelector('#removeNewLink')
 
 let options = {}
 
@@ -6,15 +7,24 @@ const restoreOptions = async () => {
 	try {
 		options = await browser.storage.local.get(defaults)
 		updateIntervalElement.value = options.updateInterval
+		removeNewLink.checked = options.removeNewLink
 	} catch (e)	{
 		console.error(e)
 	}
 }
 
-updateInterval.addEventListener('input', (e) => {
+updateInterval.addEventListener('input', e => {
 	const value = e.target.value
 	if (value !== options.updateInterval) {
 		options.updateInterval = +value
+		browser.storage.local.set(options)
+	}
+})
+
+removeNewLink.addEventListener('input', e => {
+	const value = e.target.checked
+	if (value !== options.removeNewLink) {
+		options.removeNewLink = value
 		browser.storage.local.set(options)
 	}
 })
